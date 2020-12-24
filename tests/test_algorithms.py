bb6a3b1dde8810970a19ef6fec0ae7fcfaa62524
AleksandrChirkin <chirkin2001@list.ru>
2020-12-24 20:22:06 +0500

    Updating substring (tests)

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -9,33 +9,25 @@ from benchmark import ALGORITHMS # noqa
 
 
 class AlgorithmsTest(unittest.TestCase):
-    def setUp(self) -> None:
-        self.string_io = StringIO(1000 * 'A')
-        self.string_fragments = [100 * 'A', 100 * 'A' + 'B', 'B' + 100 * 'A']
-        self.text = Path('samples/Voyna_i_mir.txt').open(encoding='1251')
-        self.text_fragments = ['князь', 'князь Андрей', 'князь Андрей Болконский']
-
     def test_for_string_ios(self) -> None:
+        string_io = StringIO(1000 * 'A')
+        string_fragments = [100 * 'A', 100 * 'A' + 'B', 'B' + 100 * 'A']
         for algorithm in ALGORITHMS:
-            for fragment in self.string_fragments:
+            for fragment in string_fragments:
                 runner = algorithm()
                 result = [index for index in
-                          runner.run(self.string_io, fragment, 10)]
+                          runner.run(string_io, fragment, 10)]
                 if fragment == 100 * 'A':
                     self.assertListEqual([i for i in range(10)], result)
                 else:
                     self.assertEqual(0, len(result))
 
     def test_for_text_files(self) -> None:
+        text = Path('samples/Voyna_i_mir.txt').open(encoding='1251')
         for algorithm in ALGORITHMS:
-            for fragment in self.text_fragments:
-                runner = algorithm()
-                result = [index for index in
-                          runner.run(self.text, fragment, 10)]
-                if fragment == 'князь Андрей Болконский':
-                    self.assertEqual(4, len(result))
-                else:
-                    self.assertEqual(10, len(result))
+            runner = algorithm()
+            result = [index for index in runner.run(text, 'князь', 10)]
+            self.assertEqual(10, len(result))
 
 
 if __name__ == '__main__':
